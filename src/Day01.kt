3af877956fa2aa53b9b6d4267d3cc5df0e2d94bc
rafal.wojcik <rafal.wojcik@schibsted.com>
2021-12-01 08:34:56 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,28 @@
 fun main() {
+    fun Sequence<Int>.countIncreased() = this
+        .windowed(2)
+        .map { (previous, next) -> if (next > previous) "inc" else "dec" }
+        .count { it == "inc" }
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.asSequence()
+            .map(String::toInt)
+            .countIncreased()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asSequence()
+            .map(String::toInt)
+            .windowed(3)
+            .map(List<Int>::sum)
+            .countIncreased()
     }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
